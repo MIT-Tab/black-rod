e8cd5d40086c420935fed8bf5bce89356c3950e8
Rodda John <rj2485@columbia.edu>
2019-10-21 23:19:14 -0400

    Continued work on webpacks

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    context: __dirname,
-    entry: './assets/js/index',
-    output: {
-	path: path.resolve('./assets/webpack_bundles/'),
-	filename: "[name]-[hash].js"
-    },
-    
-    plugins: [
-	new BundleTracker({filename: './webpack-stats.json'})
-    ]
+  mode: "none",
+  context: __dirname,
+  entry: './assets/js/index',
+  output: {
+    path: path.resolve('./assets/webpack_bundles/'),
+    filename: "[name]-[hash].js"
+  },
+  
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
 }
