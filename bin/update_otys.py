8e9e8d1e7025f6ceeeb2f181e785bb2170e3ae9f
Rodda John <rj2485@columbia.edu>
2019-11-04 07:56:32 -0500

    updated

--- bin/update_otys.py
+++ bin/update_otys.py
@@ -5,15 +5,21 @@ from core.models import Team, Debater, TOTY, NOTY, COTY, SOTY
 from core.utils.rankings import *
 
 for team in Team.objects.all():
+    print ('Updating %s' % (team,))
     update_toty(team)
     update_qual_points(team)
 
 for debater in Debater.objects.all():
+    print ('Updating %s' % (debater,))
     update_soty(debater)
     update_noty(debater)
 
 
+print ('Ranking TOTY')
 redo_rankings(TOTY.objects.filter(season=settings.CURRENT_SEASON, cache_type='toty').all())
+print ('Ranking NOTY')
 redo_rankings(NOTY.objects.filter(season=settings.CURRENT_SEASON, cache_type='noty').all())
+print ('Ranking COTY')
 redo_rankings(COTY.objects.filter(season=settings.CURRENT_SEASON, cache_type='coty').all())
+print ('Ranking SOTY')
 redo_rankings(SOTY.objects.filter(season=settings.CURRENT_SEASON, cache_type='soty').all())
