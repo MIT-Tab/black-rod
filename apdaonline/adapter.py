2169ba28b566d66106ce4d108698b3243160bde0
Rodda John <johnrod.john@gmail.com>
2021-11-16 15:49:44 -0500

    quick

--- apdaonline/adapter.py
+++ apdaonline/adapter.py
@@ -7,5 +7,5 @@ from core.models import User
 class APDAOnlineAdapter(DefaultSocialAccountAdapter):
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
-        user_field(user, 'can_view_private_videos', data.get('can_view_private_videos'))
+        user.can_view_private_videos = data.get('can_view_private_videos')
         return user
