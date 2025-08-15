459ba5e6249e2dd807202a2509a2a2bc6dc28d5a
JoeyRubas <joeymrubas@gmail.com>
2025-08-15 00:00:36 -0400

    nit+favicon

--- apda/urls.py
+++ apda/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
