cf81b20e0014ba501a371a66bfad0ffe49a627d5
JoeyRubas <joeymrubas@gmail.com>
2025-08-29 22:29:24 -0400

    bump node

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,77 @@
-var path = require("path");
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    context: __dirname,
-    
-    entry: './apda/assets/js/index',
-    
-    output: {
-	path: path.resolve('./apda/assets/webpack_bundles/'),
-	filename: "[name]-[hash].js",
-    },
-    
-    plugins: [
-	new BundleTracker({filename: 'webpack-stats.json'}),
-	new webpack.ProvidePlugin({ // inject ES5 modules as global vars
-	    $: 'jquery',
-	    Popper: 'popper.js'
-	})
-    ],
-    module: {
-	rules: [
-	    {
-		test: /\.css/,
-		use: [
-		    "css-loader"
-		]
-	    },
-	    {
-		test: /\.(less)$/,
-		use: [{
-		    loader: 'style-loader' // creates style nodes from JS strings
-		}, {
-		    loader: 'css-loader' // translates CSS into CommonJS
-		}, {
-		    loader: 'less-loader' // compiles Less to CSS
-		}],
-	    },
-	    {
-		test: /\.(scss)$/,
-		use: [{
-		    loader: 'style-loader',
-		}, {
-		    loader: 'css-loader',
-		}, {
-		    loader: 'postcss-loader',
-		    options: {
-			plugins: function () {
-			    return [
-				require('precss'),
-				require('autoprefixer')
-			    ];
-			}
-		    }
-		}, {
-		    loader: 'sass-loader'
-		}]
-	    },
-	    {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: ['babel-loader']
-	    }
-	]
-    },
-    resolve: {
-	extensions: ['*', '.js', '.jsx']
-    }
+  context: __dirname,
+
+  entry: './apda/assets/js/index',
+
+  output: {
+    path: path.resolve(__dirname, 'apda/assets/webpack_bundles/'),
+    filename: '[name]-[contenthash].js',
+    clean: true
+  },
+
+  plugins: [
+    new BundleTracker({ filename: 'webpack-stats.json' }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devtool: 'source-map'
 };
