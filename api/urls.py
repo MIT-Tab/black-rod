979a8118b47c24151e408ddcc158c9875c8e5ce7
JoeyRubas <joeymrubas@gmail.com>
2025-11-20 13:36:36 -0500

    mvp

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,12 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
+    path('standings/', views.SeasonStandingsAPIView.as_view(), name='season_standings'),
     path('schools/', views.ActiveSchoolListAPIView.as_view(), name='active_schools'),
     path('schools/all/', views.AllSchoolListAPIView.as_view(), name='all_schools'),
+    path('schools/<int:pk>/detail/', views.SchoolDetailAPIView.as_view(), name='school_detail'),
     path('debaters/<int:school_id>/', views.SchoolDebatersAPIView.as_view(), name='school_debaters'),
+    path('debaters/<int:pk>/detail/', views.DebaterDetailAPIView.as_view(), name='debater_detail'),
+    path('teams/<int:pk>/detail/', views.TeamDetailAPIView.as_view(), name='team_detail'),
+    path('tournaments/<int:pk>/detail/', views.TournamentDetailAPIView.as_view(), name='tournament_detail'),
 ]
